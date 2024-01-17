e7c91b440d7739def94aab1da86e052464ed80e0
Stx31 <kh1327303@gmail.com>
2024-01-17 15:58:33 +0100

    veriosnes tipos lugar

--- js/main.js
+++ js/main.js
@@ -2,58 +2,38 @@ const listaPokemon = document.querySelector("#listaPokemon");
 const botonesHeader = document.querySelectorAll(".btn-header");
 let URL = "https://pokeapi.co/api/v2/pokemon/";
 
-async function fetchAndDisplayPokemon(url, tipo, version) {
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-
-        if (data.types && data.game_indices) {
-            if (tipo === "ver-todos" || data.types.some(type => type.type.name === tipo)) {
-                if (!version || (version && Array.isArray(data.game_indices) && data.game_indices.some(game => game.version.name.toLowerCase() === version))) {
-                    const region = await obtenerRegion(data.game_indices);
-                    mostrarPokemon(data, region);
-                }
-            }
-        }
-    } catch (error) {
-        console.error('Error fetching Pokémon:', error);
-    }
+for (let i = 1; i <= 400; i++) {
+    fetch(URL + i)
+        .then((response) => response.json())
+        .then(data => mostrarPokemon(data))
+        .catch(error => console.error('Error fetching Pokémon:', error));
 }
 
-async function obtenerRegion(gameIndices) {
-    const versionUrl = gameIndices[0].version.url;
-    const response = await fetch(versionUrl);
-    const versionData = await response.json();
-    return versionData.main_region.name;
-}
-
-function mostrarPokemon(poke, region) {
-    let tipos = poke.types.map((type) => `<p class="${type.type.name} tipo">${type.type.name}</p>`).join('');
-
-    let pokeId = poke.id.toString().padStart(3, '0');
+function mostrarPokemon(poke) {
+    const tipos = poke.types.map(type => type.type.name).join(', ');
+    const version = poke.game_indices[0].version.name;
+    const localidad = poke.location_area_encounters;
+    const locationArea = poke.location_area_encounters;
 
     const div = document.createElement("div");
     div.classList.add("pokemon");
-
     div.innerHTML = `
-        <p class="pokemon-id-back">#${pokeId}</p>
+        <p class="pokemon-id-back">#${poke.id}</p>
         <div class="pokemon-imagen">
-            <img src="${poke.sprites.other['official-artwork'].front_default}" alt="${poke.name}">
+            <img src="${poke.sprites.other["official-artwork"].front_default}" alt="${poke.name}">
         </div>
         <div class="pokemon-info">
             <div class="nombre-contenedor">
-                <p class="pokemon-id">#${pokeId}</p>
+                <p class="pokemon-id">#${poke.id}</p>
                 <h2 class="pokemon-nombre">${poke.name}</h2>
             </div>
             <div class="pokemon-tipos">
-                Tipos: ${tipos}
+                <p class="tipos">${tipos}</p>
             </div>
-            <div class="pokemon-region">
-                Región: ${region}
-            </div>
-            <div class="pokemon-stats">
-                <p class="stat">${poke.height / 10}m</p>
-                <p class="stat">${poke.weight / 10}kg</p>
+            <div class="pokemon-version-localidad">
+                <p class="region">Version: ${version}</p>
+                <p class="localidad">Localidad: <a href="${localidad}" target="_blank">Encounters</a></p>
+                <p class="location-area">Location Area: ${locationArea}</p>
             </div>
         </div>
     `;
@@ -61,14 +41,24 @@ function mostrarPokemon(poke, region) {
 }
 
 botonesHeader.forEach(boton => boton.addEventListener("click", (event) => {
-    const tipo = event.currentTarget.dataset.type;
-    const version = event.currentTarget.dataset.version;
+    const botonId = event.currentTarget.id;
 
     listaPokemon.innerHTML = "";
 
-    (async () => {
-        for (let i = 1; i <= 300; i++) {
-            await fetchAndDisplayPokemon(URL + i, tipo, version);
-        }
-    })();
+    for (let i = 1; i <= 400; i++) {
+        fetch(URL + i)
+            .then((response) => response.json())
+            .then(data => {
+                const versiones = data.game_indices.map(game => game.version.name);
+                if ((versiones.includes(botonId) && botonId !== "versiones") || botonId === "ver-todos") {
+                    mostrarPokemon(data);
+                } else {
+                    const tipos = data.types.map(type => type.type.name);
+                    if (tipos.some(tipo => tipo.includes(botonId))) {
+                        mostrarPokemon(data);
+                    }
+                }
+            })
+            .catch(error => console.error('Error fetching Pokémon:', error));
+    }
 }));
